f626fe5e4326c9d672c8003d07647d1e661d028a
Aditya Ghosh <adi27.ghosh@gmail.com>
2024-03-13 19:45:06 +0530

    static app: Bill split value ready

--- src/App.jsx
+++ src/App.jsx
@@ -29,6 +29,7 @@ export default function App(){
         <FormAddFriend />
         <Button>Add Friend</Button>
       </div>
+      <FormSplitBill />
     </div>
   )
 }
@@ -78,6 +79,31 @@ function FormAddFriend(){
       <input type="text" />
 
       <Button>Add</Button>
+    </form>
+  )
+}
+
+function FormSplitBill(){
+  return (
+    <form className="form-split-bill">
+      <h2>Split a bill with X</h2>
+
+      <label>💰 Bill Value</label>
+      <input type="text" />
+
+      <label>🧑‍🤝‍🧑 Your expense</label>
+      <input type="text" />
+
+      <label>X's expense</label>
+      <input type="text" disabled/>
+
+      <label>🤑 Who is paying the bill</label>
+      <select>
+        <option value="user">You</option>
+        <option value="friend">X</option>
+      </select>
+
+      <Button>Split Bill</Button>
     </form>
   )
 }
