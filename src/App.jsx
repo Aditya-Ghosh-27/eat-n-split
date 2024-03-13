3cc7d00e4865c9f0373e30b178816660b81655d0
Aditya Ghosh <adi27.ghosh@gmail.com>
2024-03-13 19:26:12 +0530

    building the static app: Add friend component

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { Children } from "react";
+
 const initialFriends = [
   {
     id: 118836,
@@ -24,6 +26,8 @@ export default function App(){
     <div className="app">
       <div className="sidebar">
         <FriendsList />
+        <FormAddFriend />
+        <Button>Add Friend</Button>
       </div>
     </div>
   )
@@ -53,7 +57,27 @@ function Friend({ friend }){
 
       {friend.balance === 0 && <p>You and {friend.name} are even</p>}
 
-      <button className="button">Select</button>
+      <Button>Select</Button>
     </li>
+  )
+}
+
+function Button({children}){
+  return (
+    <button className="button">{children}</button>
+  )
+}
+
+function FormAddFriend(){
+  return (
+    <form className="form-add-friend">
+      <label>🧑‍🤝‍🧑Friend Name: </label>
+      <input type="text"/>
+
+      <label>🌇 Image URL</label>
+      <input type="text" />
+
+      <Button>Add</Button>
+    </form>
   )
 }
